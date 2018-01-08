eae9cd117286c982d8b42b29badfeb1e9e63cc69
MinJu <kmju1997@gmail.com>
2018-01-08 23:33:45 +0900

    csharp test

---
+++ test1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace csharptest
+{
+    public static class Palindrome{
+
+        public static bool IsPalindrome(string word){
+
+        bool result;
+        char [] arr = word.ToCharArray();
+        Array.Reverse(arr);
+        string reversed = new string(arr);
+
+        result = word.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+        
+        return result;
+        }
+
+        public static void Main(string[] args){
+            Console.WriteLine(Palindrome.IsPalindrome("devLeveled"));
+        }
+
+    }
+}
---
+++ test2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class TextInput
+    {
+
+        protected string s = "";
+
+        public virtual void Add(char c)
+        {
+        
+            string a = Convert.ToString(c);
+
+            this.s = String.Concat(this.s,a);
+
+        }
+
+        public string GetValue()
+        {
+
+            return this.s;
+
+        }
+
+
+    }
+
+    public class NumericInput : TextInput
+    {
+
+        public override void Add(char c)
+        {
+
+            if (Char.IsNumber(c))
+            {                 
+                string a = Convert.ToString(c);
+
+                this.s = String.Concat(this.s, a);
+
+
+            }
+
+            else return;
+        }
+
+
+
+
+
+    }
+
+    public class UserInput
+    {
+        public static void Main(string[] args)
+        {
+            TextInput input = new NumericInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('0');
+            Console.WriteLine(input.GetValue());
+
+        }
+
+    }
+}
